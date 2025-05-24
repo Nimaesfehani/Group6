18ffadc0ba3df4e387a52c82f2e1c1d877cde75f
farhanbgh <farhanbagheri.fb@gmail.com>
2025-05-24 11:06:30 +0330

    Add Humhub project files into Phase-2 folder

---
+++ Phase-2/protected/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=humhub_db',  // نام پایگاه داده خود را وارد کنید
+    'username' => 'root',  // نام کاربری پایگاه داده (پیش‌فرض در XAMPP 'root' است)
+    'password' => '',      // رمز عبور پایگاه داده (پیش‌فرض در XAMPP خالی است)
+    'charset' => 'utf8',
+];
